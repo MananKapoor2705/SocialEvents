edc4ff31c74c3407483a31d476bbf7463fd911b2
Manan Kapoor <manan.kapoor@nineleaps.com>
2021-02-27 01:26:02 +0530

    Activity CRUD Op's and AutoMapper Implemented.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,22 +11,35 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity newActivity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { activity = newActivity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateActivity(Activity updatedActivity, Guid id)
+        {
+            updatedActivity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { updatedActivity = updatedActivity }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
---
+++ Application/Activities/Create.cs
@@ -0,0 +1,31 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity activity { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                await _context.Activities.AddAsync(request.activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
